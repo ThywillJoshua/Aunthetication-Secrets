0f57b83e5b161daa07b99c484f3430909b50582c
ThywillJoshua <76951731+ThywillJoshua@users.noreply.github.com>
2021-09-10 12:18:51 +0200

    Add env files

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 // const ejs = require("ejs");
@@ -16,8 +17,10 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const secret = "Thisisourlittlesecret";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ["password"],
+});
 
 const User = new mongoose.model("User", userSchema);
 
